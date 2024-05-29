118af894369fba35d3cb9ba57211f691f0866a43
nicholas koyi <koyinicholas@gmail.com>
2024-05-29 21:42:02 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 def return_evens(num_list):
-    pass
+    
+    even_numbers = [num for num in num_list if num % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list: 
+        return [] 
+    else:
+    
+        exclamated_sentences = [sentence + '!' for sentence in sentence_list]
+        return exclamated_sentences
